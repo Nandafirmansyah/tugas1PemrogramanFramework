08490860b060cfcebd3b8a4defe0ecd21b2329ef
Nandafirmansyah <nandafirman9@gmail.com>
2017-03-03 08:39:01 +0700

    versi 1.0

---
+++ application/views/about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>About</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+
+			<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			
+	
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+					<li><a class="glyphicon glyphicon-home" href ="<?php echo site_url('home') ?>">/HOME<br></a></li>
+					<li><a class="glyphicon glyphicon-book" href="<?php echo site_url('about') ?>">/ABOUT</a></li>
+					<li><a class="glyphicon glyphicon-phone" href="<?php echo site_url('contact') ?>">/CONTACT</a></li>
+				</ul>
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+				<ul class="nav navbar-nav navbar-right">
+			
+					<li class="dropdown">
+						<a href="#" class="glyphicon glyphicon-tasks" data-toggle="dropdown"><b class=""></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="<?php echo site_url('home') ?>">Home</a></li>
+							<li><a href="<?php echo site_url('about') ?>">About</a></li>
+							<li><a href="<?php echo site_url('contact') ?>">Contact</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+			<div class="jumbotron">
+			<div class="container">
+			<div class="row">
+  		<div class="col-xs-6 col-md-70">
+   			<a class="thumbnail">
+     		<img src="https://iannkyu.files.wordpress.com/2012/06/logo-polinema-final-2011.jpg">
+    		</a>
+		</div>
+  <form class="navbar-form navbar-left" role="search">
+				<h1>BIODATA :</h1>
+				
+			<ul class="list-group">
+  				<li class="list-group-item">Nama		: Nanda firmansyah</li>
+ 			 	<li class="list-group-item">NIM 		: 1541180036</li>
+  				<li class="list-group-item">No.Telp 	: 085850080250</li>
+  				<li class="list-group-item">Alamat 	: Jln.Ikan Gurami No.3 Malang</li>
+  				<li class="list-group-item">E-Mail 	: nandafirman9@gmail.com</li>
+			</ul>
+				
+			</div>
+		</div>
+			
+		</p>
+		</div>
+			</div>
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
